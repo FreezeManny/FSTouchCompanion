733ffbeed0ba2e7c8ca5007c84425ef430a7ea40
Manny Freeze <freezemanny@gmail.com>
2024-09-08 15:18:33 +0200

    Fix flightplan fullscreen

--- prod/src/routes/flightplan/+page.svelte
+++ prod/src/routes/flightplan/+page.svelte
@@ -1,35 +1,30 @@
 <script lang="js">
-  
   import { onMount } from "svelte";
 
-
   let data;
-  var pdfLink;
+  let pdfLink = "";
 
   onMount(async () => {
-    // Code to execute on page load
-    console.log("Hello World")
+    console.log("Hello World");
     try {
       const response = await fetch(
         "https://www.simbrief.com/api/xml.fetcher.php?username=" +
-          "aplayz"+ //settings.simbriefUsername
+          "aplayz" + //settings.simbriefUsername
           "&json=1",
       );
       data = await response.json();
+      pdfLink = data.files.directory + data.files.pdf.link;
+      console.log("Link: " + pdfLink);
     } catch (error) {
       console.error("Failed to fetch data:", error);
     }
-    //console.log(data);
-    pdfLink = data.files.directory + data.files.pdf.link;
-    console.log("Link: " + pdfLink);
   });
-
 </script>
 
-
-<iframe
-title="flightplan"
-src={pdfLink}
-style="border: none;"
-class="w-full h-full"
-/>
+<div class="w-screen h-screen flex items-center justify-center">
+  <iframe
+    title="flightplan"
+    src={pdfLink}
+    class="w-full h-full border-none"
+  />
+</div>
