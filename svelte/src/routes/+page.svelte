c45e1d5ea974e2edc20a88a01daae026f8e48629
Manny Freeze <freezemanny@gmail.com>
2024-09-13 13:45:30 +0200

    First Flightcard on mainpage commit

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="js">
   import { settings, simbriefData } from "$lib/stores";
   import { getToastStore } from "@skeletonlabs/skeleton";
+  import { goto } from "$app/navigation";
 
   const toastStore = getToastStore();
 
@@ -12,6 +13,7 @@
   });
 
   async function getFlightPlan() {
+    console.log("Fetching flight plan...");
     const fetchURL = "https://www.simbrief.com/api/xml.fetcher.php?username=" + $settings.simbriefUsername + "&json=1";
     try {
       const response = await fetch(fetchURL);
@@ -19,24 +21,84 @@
 
       if (data.fetch.status == "Success") {
         $simbriefData = data;
-	} else {
-		$simbriefData = null;
+      } else {
+        $simbriefData = null;
         toastStore.trigger(simbriefError("Simbrief: " + data.fetch.status));
       }
     } catch (error) {}
   }
+  let flightNumber = "";
+  let departure = "";
+  let arrival = "";
+  let aircraft = "";
+  let date = "";
+
+  $: if ($simbriefData) {
+    flightNumber = $simbriefData.atc.callsign;
+    departure = { code: $simbriefData.origin.icao_code, name: $simbriefData.origin.name };
+    arrival = { code: $simbriefData.destination.icao_code, name: $simbriefData.destination.name };
+    aircraft = $simbriefData.aircraft.name;
+    date = new Date($simbriefData.params.time_generated * 1000).toUTCString();
+  } else {
+    flightNumber = "XXXX";
+    departure = { code: "", name: "XXXX" };
+    arrival = { code: "XXXX", name: "XXXX" };
+    aircraft = "XXXX";
+    date = "XXXX";
+  }
 </script>
 
-<h1 class="h1">Homepage</h1>
-
-<button type="button" class="btn variant-filled" on:click={getFlightPlan}>Load Flight</button>
-<h1 class="h1">
+<div class="card p-6 max-w-md mx-auto my-5">
   {#if $simbriefData}
-  Flightplan Loaded
+    <!-- Date -->
+    <div>
+      <div class="flex items-center justify-between mb-4">
+        <div class="text-left text-surface text-sm mb-2">Latest Flight Plan:</div>
+        <div class="text-right text-surface text-sm mb-2">{date}</div>
+      </div>
+    </div>
+
+    <!-- Flight Number -->
+    <div class="text-center text-2xl">{flightNumber}</div>
+
+    <!-- Departure / Arrival Section -->
+    <div class="flex items-center justify-between mb-4">
+      <!-- Departure Info -->
+      <div class="card p-4 variant-filled-tertiary">
+        <div class="text-center">
+          <div class="text-lg font-bold">{departure.code}</div>
+          <div class="text-surface text-sm">{departure.name}</div>
+        </div>
+      </div>
+
+      <!-- Arrival Info -->
+      <div class="card p-4 variant-filled-tertiary">
+        <div class="text-center">
+          <div class="text-lg font-bold">{arrival.code}</div>
+          <div class="text-surface text-sm">{arrival.name}</div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Aircraft Type -->
+    <div class="flex flex-col items-center justify-center my-5">
+      <div class="text-surface text-sm mb-1">Aircraft Type:</div>
+      <div class="text-center text-lg">
+        {aircraft}
+      </div>
+    </div>
   {:else}
-  Flightplan not loaded
+    <aside class="alert variant-filled-warning m-5">
+      <!-- Message -->
+      <div class="alert-message">
+        <h3 class="h3">Load a valid Flightplan</h3>
+      </div>
+    </aside>
   {/if}
-</h1>
 
-
-<div class="card">Basic</div>
+  <!-- Action Buttons -->
+  <div class="flex justify-around">
+    <button class="btn variant-filled" on:click={getFlightPlan}>Load Flightplan</button>
+    <button class="btn variant-filled" on:click={() => goto("/flightplan")}>View Flightplan</button>
+  </div>
+</div>
---
+++ svelte/src/routes/test/Flightcard.svelte
@@ -0,0 +1,53 @@
+<script>
+  export let flightNumber = "HLF5MF";
+  export let departure = { code: "EDDS", city: "Stuttgart" };
+  export let arrival = { code: "EDDF", city: "Frankfurt/Main" };
+  export let aircraft = "B737-800";
+  export let aircraftType = "ZIBO";
+  export let date = "11 Sep 2024 - 18:49 UTC";
+</script>
+
+<!-- Card Container -->
+<div class="card p-6 max-w-md mx-auto">
+  <!-- Date -->
+  <div>
+    <div class="flex items-center justify-between mb-4">
+      <div class="text-left text-surface text-sm mb-2">Latest Flight Plan:</div>
+      <div class="text-right text-surface text-sm mb-2">{date}</div>
+    </div>
+  </div>
+
+  <!-- Flight Number -->
+  <div class="text-center text-2xl">{flightNumber}</div>
+
+  <!-- Departure / Arrival Section -->
+  <div class="flex items-center justify-between mb-4">
+    <!-- Departure Info -->
+    <div class="card p-4 variant-filled-tertiary">
+
+    <div class="text-center">
+      <div class="text-lg font-bold">{departure.code}</div>
+      <div class="text-surface text-sm">{departure.city}</div>
+    </div>
+    </div>
+
+    <!-- Arrival Info -->
+    <div class="card p-4 variant-filled-tertiary">
+      <div class="text-center">
+        <div class="text-lg font-bold">{arrival.code}</div>
+        <div class="text-surface text-sm">{arrival.city}</div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Aircraft Type -->
+  <div class="text-center text-lg my-4">
+    {aircraft}
+  </div>
+
+  <!-- Action Buttons -->
+  <div class="flex justify-around">
+    <button class="btn variant-filled">Load Flightplan</button>
+    <button class="btn variant-filled">View Flightplan</button>
+  </div>
+</div>
