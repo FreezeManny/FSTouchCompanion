ee7b11350396531f59a3ec58e3be4b58848f9e98
Manuel Fritz <freezemanny@gmail.com>
2025-11-16 13:22:11 +0100

    Revert

--- fsConnect/main.go
+++ fsConnect/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"embed"
+	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -11,7 +14,46 @@ import (
 //go:embed all:frontend/build
 var assets embed.FS
 
+//go:embed SimConnect.dll
+var simConnectDLL []byte
+
+// extractSimConnectDLL extracts the embedded SimConnect.dll to the application directory
+func extractSimConnectDLL() error {
+	// Get the executable directory
+	exePath, err := os.Executable()
+	if err != nil {
+		return err
+	}
+	exeDir := filepath.Dir(exePath)
+	dllPath := filepath.Join(exeDir, "SimConnect.dll")
+
+	// Check if DLL already exists
+	if _, err := os.Stat(dllPath); err == nil {
+		// DLL exists, check if it needs updating by comparing sizes
+		info, _ := os.Stat(dllPath)
+		if int(info.Size()) == len(simConnectDLL) {
+			log.Println("SimConnect.dll already exists and is up to date")
+			return nil
+		}
+	}
+
+	// Write the embedded DLL to disk
+	log.Printf("Extracting SimConnect.dll to %s", dllPath)
+	err = os.WriteFile(dllPath, simConnectDLL, 0644)
+	if err != nil {
+		return err
+	}
+
+	log.Println("SimConnect.dll extracted successfully")
+	return nil
+}
+
 func main() {
+	// Extract SimConnect.dll before starting the app
+	if err := extractSimConnectDLL(); err != nil {
+		log.Printf("Warning: Failed to extract SimConnect.dll: %v", err)
+	}
+
 	// Create an instance of the app structure
 	app := NewApp()
 
