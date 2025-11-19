d6ba64018fd08285cb68bf7ac43a9cfad3505e35
Manuel Fritz <freezemanny@gmail.com>
2025-11-19 13:15:46 +0100

    Added Proper Break Objects

--- svelte/src/types/checklist.ts
+++ svelte/src/types/checklist.ts
@@ -1,11 +1,17 @@
 export type ChecklistTuple = [string, string];
+
+export type ChecklistBreak = { break: true };
+
 export type ChecklistObject = {
   key: string;
   value?: string;
   subitems?: ChecklistTuple[];
 };
-export type ChecklistItem = ChecklistTuple | ChecklistObject;
+
+export type ChecklistItem = ChecklistTuple | ChecklistObject | ChecklistBreak;
+
 export type ChecklistSection = ChecklistItem[];
+
 export type ChecklistData = {
   info: {
     name: string;
