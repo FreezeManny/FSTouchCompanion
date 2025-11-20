5ac6129b1c6336c31e7146927aa4907910bf4894
Manuel Fritz <freezemanny@gmail.com>
2025-11-20 13:57:05 +0000

    Tailwind Recognized

--- svelte/src/app.d.ts
+++ svelte/src/app.d.ts
@@ -1,20 +1,12 @@
-// See https://kit.svelte.dev/docs/types#app
+// See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
-// and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
-}
-
-// Extend global interfaces
 declare global {
-	// Add custom global types here
-	
-	// Window interface extensions (if needed)
-	interface Window {
-		// Add window extensions here
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
 	}
 }
 
