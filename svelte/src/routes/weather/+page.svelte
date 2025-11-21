048f938a417751c608ef0f46e71a2bd4c13a06cc
Manuel Fritz <freezemanny@gmail.com>
2025-11-21 10:05:08 +0100

    Added Lucide Svelte Object

--- svelte/src/routes/weather/+page.svelte
+++ svelte/src/routes/weather/+page.svelte
@@ -2,6 +2,7 @@
   import { onMount, onDestroy } from "svelte";
   import { get } from "svelte/store";
   import { selectedAirports, settings, simbriefData } from "$lib/stores";
+  import { RefreshCcw } from "@lucide/svelte";
   import { toaster } from "$lib/toaster";
 
   type SimbriefError = {
@@ -191,20 +192,7 @@
   <div class="grid grid-cols-2">
     <button type="button" class="btn preset-filled mx-1" onclick={simbriefButtonHandler}>Simbrief</button>
     <button type="button" class="btn preset-filled mx-1" onclick={updateButtonHandler} aria-label="Update Weather">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="size-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-        />
-      </svg>
+        <RefreshCcw />
     </button>
   </div>
 </div>
