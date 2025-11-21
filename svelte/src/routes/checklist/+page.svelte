eb28ebc7ebf74af89d152b050a6f52f080db77fe
Manuel Fritz <freezemanny@gmail.com>
2025-11-21 14:41:12 +0000

    Fixed Modal not closing

--- svelte/src/routes/checklist/+page.svelte
+++ svelte/src/routes/checklist/+page.svelte
@@ -149,22 +149,26 @@
         <Popover.Positioner class="z-20!">
           <Popover.Content class="rounded-lg shadow-xl bg-neutral-900 border border-neutral-700 p-0 min-w-[0] w-fit">
             <div class="flex flex-col">
-              <button
-                type="button"
-                class="w-full text-left px-3 py-2 text-base text-white border-b border-neutral-700 hover:bg-neutral-800 transition"
-                style="border-radius: 8px 8px 0 0;"
-                onclick={resetSection}
-              >
-                Reset Section
-              </button>
-              <button
-                type="button"
-                class="w-full text-left px-3 py-2 text-base text-white border-t border-neutral-700 hover:bg-neutral-800 transition"
-                style="border-radius: 0 0 8px 8px;"
-                onclick={resetAircraft}
-              >
-                Reset Entire Aircraft
-              </button>
+              <Popover.CloseTrigger>
+                <button
+                  type="button"
+                  class="w-full text-left px-3 py-2 text-base text-white border-b border-neutral-700 hover:bg-neutral-800 transition"
+                  style="border-radius: 8px 8px 0 0;"
+                  onclick={resetSection}
+                >
+                  Reset Section
+                </button>
+              </Popover.CloseTrigger>
+              <Popover.CloseTrigger>
+                <button
+                  type="button"
+                  class="w-full text-left px-3 py-2 text-base text-white border-t border-neutral-700 hover:bg-neutral-800 transition"
+                  style="border-radius: 0 0 8px 8px;"
+                  onclick={resetAircraft}
+                >
+                  Reset Entire Aircraft
+                </button>
+              </Popover.CloseTrigger>
             </div>
             <Popover.Arrow style="--arrow-size: 10px; --arrow-background: #171923;">
               <Popover.ArrowTip />
