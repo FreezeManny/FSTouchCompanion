d514dfc1eb9cd37346bff3b291d903e1fbd355f3
Manuel Fritz <freezemanny@gmail.com>
2025-11-21 10:20:49 +0100

    Updated Button style

--- svelte/src/routes/flightplan/+page.svelte
+++ svelte/src/routes/flightplan/+page.svelte
@@ -58,14 +58,14 @@
     <!-- Floating Zoom Controls -->
     <div class="fixed bottom-4 right-4 flex flex-col gap-2 z-50">
       <button
-        class="btn preset-filled w-12 h-12 rounded-full shadow-lg"
+        class="btn preset-filled w-9 h-9 shadow-lg"
         onclick={increaseFontSize}
         title="Increase font size"
       >
         +
       </button>
       <button
-        class="btn preset-filled w-12 h-12 rounded-full shadow-lg"
+        class="btn preset-filled w-9 h-9 shadow-lg"
         onclick={decreaseFontSize}
         title="Decrease font size"
       >
